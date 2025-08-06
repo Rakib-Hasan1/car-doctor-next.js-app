8d650fef91613387df31c0da5e42cc22a6d807a4
rakib hasan <dev.rakibhasan1@gmail.com>
2025-08-06 15:31:04 +0600

    added about and services page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+        <h1 className='font-bold text-3xl text-center'>This is About page</h1>
+    </div>
+  )
+}
---
+++ src/app/services/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ServicesPage() {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold text-center'>This is services page</h1>
+    </div>
+  )
+}
